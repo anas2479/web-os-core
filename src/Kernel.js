058d5565805e5ffccde33734a3e532b489a96b3b
Anas Mohamed <amohamed2479@gmail.com>
2021-11-07 21:10:08 -0600

    create the Kernel constructor

---
+++ src/Kernel.js
@@ -0,0 +1,16 @@
+import { Filesystem } from "./Filesystem";
+
+export default class Kernel{
+    constructor(config){
+
+        this.filesystem = new Filesystem(config.files)
+        this.apps = config.apps || []
+        this.plugins = []
+    },
+
+
+    registerPlugin(plugin){
+        plugins.push(plugin)
+    }
+}
+
